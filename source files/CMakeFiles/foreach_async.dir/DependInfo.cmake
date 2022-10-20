173977d4f49d7325cd29d5ac41d257102fbb7aa9
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2022-10-19 20:35:26 -0400

    Add files via upload

---
+++ source files/CMakeFiles/foreach_async.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/work/chuanqiu/senderReceiver/workhpx/test_async/foreach_async.cpp" "CMakeFiles/foreach_async.dir/foreach_async.cpp.o" "gcc" "CMakeFiles/foreach_async.dir/foreach_async.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ source files/CMakeFiles/foreach_async.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/foreach_async.dir/foreach_async.cpp.o"
+  "CMakeFiles/foreach_async.dir/foreach_async.cpp.o.d"
+  "foreach_async"
+  "foreach_async.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/foreach_async.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
